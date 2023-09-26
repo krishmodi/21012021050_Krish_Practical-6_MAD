a3a9317d7521017653abf40447c8093a9780b89a
krish <krishpmodi@gmail.com>
2023-09-26 12:17:31 +0530

    Initial commit

---
+++ app/src/test/java/com/krishmodi/a21012021050_madp6/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.krishmodi.a21012021050_madp6
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
